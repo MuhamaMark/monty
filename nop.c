5f0b21d89931b9b5c56b6b5b4f245e7df9155b0c
muhama mark <muhamamark@gmail.com>
2022-10-07 02:12:33 +0300

    Create nop.c

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * m_nop - no operation performed
+ * @stack: double pointer to the head of the stack
+ * @line_no: line number of current operation
+ * Return: void
+ */
+void m_nop(stack_t **stack, unsigned int line_no)
+{
+	(void)stack;
+	(void)line_no;
+}
